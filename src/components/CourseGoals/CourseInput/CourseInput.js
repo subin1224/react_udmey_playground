0c470d95269b677557541c407700782c1f1fff77
subinLee <salllena33@gmail.com>
2023-04-26 15:44:55 +0900

    04-25, 04-26 - styled components, CSS Module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,26 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// import styled from "styled-components";
 
-  const goalInputChangeHandler = event => {
+// styled-components 에서 props를 이용하여 동적으로 스타일을 주는 방법
+// 백틱 구문, 화살표 함수를 이용하여 작성 할 수 있다.
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* CSS 스타일을 동적으로 변환하기 - inline style보다는 벡틱을 이용하여 동적으로 이용 */}
+      {/* <FormControl invalid={!isValid}> */}
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
