8cc1ba7433169a9ccae5c06d4f28fab0396f9727
subinLee <salllena33@gmail.com>
2023-05-12 20:52:54 +0900

    05-12 ############################### 컴포넌트 업데이트 실행시 변경점 확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+const [showParagraph, setShowParagraph] = useState(false);
+
+// 첫 렌더링 때 실행 & 버튼을 누르면 실행
+// 실제 DOM을 통한 업데이트는 가상 스냅샷 간의 차이점만 반영함.
+console.log('APP RUNNING');
+
+const toggleParagraphHandler = () => {
+  setShowParagraph(prev => !prev);
+}
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
