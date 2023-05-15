4b8d631cee99148ea5b7da90c20d16fdbc4bcd9c
subinLee <salllena33@gmail.com>
2023-05-15 19:09:26 +0900

    05-15 ###################### useMemo, useCallback

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,9 +1,22 @@
 import React from "react";
+import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
 
-  return <p>{props.show ? "This is new!" : ""}</p>;
+  // props 가 바뀌어서 해당 컴포넌트 평가가 이뤄지는 것이 아닌 부모 컴포넌트에서 특성이 바뀌어도 재평가 한다.
+  // => toggleParagraphHandler
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+// React.memo
+/*
+인자로 들어간 컴포넌트에 어떤 props 가 입력되는지 확인하고 
+입력되는 모든 props의 신규 값을 확인한 뒤 이를 기존의 props의 값과 비교하도록 리액트에게 전달
+props의 값이 바뀐 경우에만 컴포넌트를 재실행 및 재평가
+
+-> 최적하에는 비용이 따르기 때문에 모든 컴포넌트에 하지 않는다.
+기존의 props값 저장 할 공간 필요, 비교할 작업도 필요. => 이런 성능화 작업은 어떤 컴포넌트에 쓰이는지에 따라 성능이 달라짐
+ex. 컴포넌트 트리가 매우 큰 경우에 유용하게 사용 or 컴포넌트 트리의 상위에 있다면 유용하게 사용
+*/
+export default React.memo(DemoOutput);
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
+  console.log('Button RUNNING');
   return (
     <button
       type={props.type || 'button'}
@@ -15,4 +16,15 @@ const Button = (props) => {
   );
 };
 
-export default Button;
+/*
+toggleParagraphHandler 함수는 재 생성 되므로 React.memo() 로 감싸주어도 해당 컴포넌트는 계속 재평가가 된다.
+
+배열이나 객체 함수 
+[1, 2, 3] === 1, 2, 3] => false
+함수는 하나의 객체에 불과.
+App 함수가 실행될 떄 마다 새로운 함새 객체가 생성이 되고 그 함수 객체가 onClick props에 전달이 된다.
+예를 들어 props.onClick === props.previous.onClick 은 false가 나온다 .
+
+값이 변경되었다고 받아들이기 때문에 해당 컴포넌트는 계속 재평가 재실행이 된다. -- 중요 !!
+*/
+export default React.memo(Button);
