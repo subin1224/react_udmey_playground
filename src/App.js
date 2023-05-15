a9510f3079413a5fbafb27e01ee3b0c668900a58
subinLee <salllena33@gmail.com>
2023-05-15 15:06:19 +0900

    05-15 ############### 163강의 다시 시작

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import Button from './components/UI/Button/Button';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-const [showParagraph, setShowParagraph] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
-// 첫 렌더링 때 실행 & 버튼을 누르면 실행
-// 실제 DOM을 통한 업데이트는 가상 스냅샷 간의 차이점만 반영함.
-console.log('APP RUNNING');
+  // 첫 렌더링 때 실행 & 버튼을 누르면 실행
+  // 실제 DOM을 통한 업데이트는 가상 스냅샷 간의 차이점만 반영함.
+  console.log("APP RUNNING");
 
-const toggleParagraphHandler = () => {
-  setShowParagraph(prev => !prev);
-}
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prev) => !prev);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
